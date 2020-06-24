b422003db2ca4d001f99e18d518ae955696293b2
Mansi Mittal <59113081+cn-mm@users.noreply.github.com>
2020-06-24 16:42:00 +0530

    Add files via upload
    minor changes and code cleanup

--- Web_Scraper/2t.py
+++ Web_Scraper/2t.py
@@ -1,3 +1,4 @@
+# CURRENT WORKING CAPACITY IS 20 IMAGES 
 import cv2
 import re
 import webbrowser
@@ -8,10 +9,8 @@ from urllib.request import Request, urlopen
 import bs4 as bs
 import funcs
 
-AGENT = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
 
-
-query = 'synesthesia'
+query = 'kittens'
 url = 'https://www.google.co.in/search?q='+query+'&source=lnms&tbm=isch'
 
 # url = 'http://www.gutenberg.org/cache/epub/1112/pg1112.txt'
@@ -28,6 +27,7 @@ soup = bs.BeautifulSoup(res.text, 'lxml')
 print(type(res.text))
 
 '''
+# HELPER STATEMENTS FOR UNDERSTANDING WHAT IS BEING RETURNED 
 images = soup.select('div img')
 print(len(images))  # returning only only 20 images 
 print(type(images))
@@ -35,15 +35,13 @@ print(images[-1])   # prints the class and the src
 print(images[10].attrs)
 for i in range(len(images)):
     print(images[i].get('src'))    # gives link to the site where the image can be found 
-
 # print(type(soup.encode("utf-8")))
 
-#(res.text[:200].encode("utf-8")) # gives me bytes 
 
 '''
 
-#making new folder and deleting prev one 
-os.makedirs('new')
+# Making new folder 
+os.makedirs('new1')
 
 
 imgElem = soup.select('div img')     #getting img tag under  comic divison
@@ -54,7 +52,6 @@ for i in range(1,len(imgElem)):
 
     else:
         try:
-            print("Inside try block")
             imgUrl = imgElem[i].get('src')
             print(imgElem[i].get('src'))
             print('Downloading image %s.....' %(imgUrl))
@@ -66,8 +63,8 @@ for i in range(1,len(imgElem)):
         #skip if not a normal image file
             print(e)
             
-        num = str(i)
-        imageFile = open(os.path.join('.\\new', num),'wb')     #write  downloaded image to hard disk
+        num = str(i) + "jpg"
+        imageFile = open(os.path.join('.\\new1', num),'wb')     #write  downloaded image to hard disk
         for chunk in res.iter_content(10000):
             imageFile.write(chunk)
             
@@ -75,6 +72,8 @@ for i in range(1,len(imgElem)):
 
 
 '''
+URLLIB ALTERNATIVE 
+AGENT = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
 
 res1 = Request(url, headers= AGENT)
 print(type(res1))
